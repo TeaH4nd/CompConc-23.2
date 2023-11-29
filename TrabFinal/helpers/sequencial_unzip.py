c168ae64344e4d6db3931edf82fe6ef6378b36e0
TeaH4nd <alexchamon@poli.ufrj.br>
2023-11-28 21:20:49 -0300

    better sequencitial unzip

--- TrabFinal/helpers/sequencial_unzip.py
+++ TrabFinal/helpers/sequencial_unzip.py
@@ -4,7 +4,9 @@ import zipfile
 def sequential_extraction(zip_file_path, extract_folder):
     try:
         with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-            zip_ref.extractall(extract_folder)
+            file_names = zip_ref.namelist()
+            for file_name in file_names:
+                zip_ref.extract(file_name, extract_folder)
         # print(f"Successfully decompressed '{zip_file_path}' to '{extract_folder}'.")
     except zipfile.BadZipFile:
         print(f"Error: '{zip_file_path}' is not a valid ZIP file.")
